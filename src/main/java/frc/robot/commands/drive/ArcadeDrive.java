048be34bfddb30e6e555d9bafbdf21f035b1d543
Alx424 <knowledgeablehamster@gmail.com>
2022-11-17 17:08:08 -0500

    Add command for drivetrain

---
+++ src/main/java/frc/robot/commands/drive/ArcadeDrive.java
@@ -0,0 +1,20 @@
+package frc.robot.commands.drive;
+
+import java.util.Set;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.Drivetrain;
+
+public class ArcadeDrive implements Command {
+    Drivetrain drive;
+    public ArcadeDrive(Drivetrain drive) {
+        this.drive = drive;
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        // TODO Auto-generated method stub
+        return Set.of(drive);
+    }
+}
